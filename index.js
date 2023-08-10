42a428550e5d5c229189f8caba91bdba7bb1296a
salamanderctesiphon <swbrookshire@gmail.com>
2023-08-10 14:11:37 -0500

    implemented the function that allows the user to navigate to the different pages on the site

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+  if(req.url === '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  } else if(req.url === '/about') {
+    fs.readFile('about.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  } else if(req.url === '/contact-me') {
+    fs.readFile('contact-me.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  } else {
+    fs.readFile('404.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  }
 }).listen(8080);
